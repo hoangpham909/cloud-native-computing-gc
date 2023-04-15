e93141836f7eb72f880f31d109a265ac02365fde
David Lindgren Kamali <david.lindgren.kamali@outlook.com>
2023-04-15 03:27:37 +0200

    Lagt till spelare  team och exempeldata

---
+++ structs/player.go
@@ -0,0 +1,18 @@
+package player
+
+type Player struct {
+	ID           int
+	Name         string
+	TeamID       int
+	Team         Team
+	JerseyNumber int
+	BirthYear    int
+}
+
+var forenames = [20]string{"Anna", "Lisa", "Emil", "Carl", "Rick",
+	"Elon", "Steve", "Juli", "Filip", "Stefan", "Anders", "Annika",
+	"Lars", "Mick", "Carola", "Thea", "Lola", "Stig", "Josefin", "Helmer"}
+
+var surnames = [20]string{"Lindgren", "Lindqvist", "Larsson", "Eliasson", "Häggkvist", "Andersson", "Svensson",
+	"Höglund", "Löfven", "Thunberg", "Gustafsson", "Djarin", "Skywalker", "Jones", "Wayne", "Ågren", "Munther",
+	"Maggio", "Timell", "Johansson"}
---
+++ structs/team.go
@@ -0,0 +1,17 @@
+package team
+
+type Team struct {
+	ID          int
+	Name        string
+	City        string
+	FoundedYear int
+}
+
+var teams = [20]string{"Hawks", "Eagels", "Bulls", "Titans",
+	"Sharks", "Goats", "Vikings", "Lions", "Giants", "Falcons",
+	"Parrots", "Dolphins", "Panthers", "Jaguars", "Capybaras",
+	"Dinosaurs", "Rocks", "Meteors", "Alligators", "Raiders"}
+
+var cities = [20]string{"Stockholm", "Linköping", "Göteborg", "Varberg", "Halmstad", "Lund", "Jönköping", "Malmö",
+	"Helsingborg", "Luleå", "Kungsbacka", "Borås", "Alingsås", "Uppsala", "Örebro", "Umeå", "Norrköping", "Gävle",
+	"Växjö", "Sundsvall"}
