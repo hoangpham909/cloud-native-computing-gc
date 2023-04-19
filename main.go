98b4936d466bb92cf887513d54e85bf370fcbc06
David Lindgren Kamali <david.lindgren.kamali@outlook.com>
2023-04-19 15:04:44 +0200

    lagt till go fil för person api samt david api

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	persons "GC/apis"
 	ginning "GC/gin"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,7 @@ func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", ginning.Home)
+	r.GET("/api/david", persons.David)
 	r.Run(":8080")
 
 
