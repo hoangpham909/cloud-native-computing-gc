0e85a1ff9074d7c4a0e550a745783469bfa5d0a2
David Lindgren Kamali <david.lindgren.kamali@outlook.com>
2023-04-19 14:53:20 +0200

    lagt till startstida med gin

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	ginning "GC/gin"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", ginning.Home)
+	r.Run(":8080")
+
 
 }
